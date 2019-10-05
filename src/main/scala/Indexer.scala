39676615a7c8ced7f32a01684b6c777aecfb128b
arseniy <3jib.mu4o@gmail.com>
2019-10-05 05:37:52 +0300

    Added JSON parsing functions and unifier for text

--- src/main/scala/Indexer.scala
+++ src/main/scala/Indexer.scala
@@ -1,4 +1,7 @@
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
+import scala.util.parsing.json.JSON.parseFull
+
 
 object Indexer {
   def main(args: Array[String]): Unit = {
@@ -7,5 +10,15 @@ object Indexer {
 
     val inputPath = args(0)
     val outputPath = args(1)
+  }
+
+  def standardize(word: String): String = {
+    word.replaceAll("""('s)|([\p{Punct}&&[^-]])""", " ")
+      .trim
+      .toLowerCase
+  }
+
+  def parseJson(jsonString: String): Map[String, String] = {
+    parseFull(jsonString).get.asInstanceOf[Map[String, String]]
   }
 }
