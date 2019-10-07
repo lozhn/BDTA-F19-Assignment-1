da81f5bf7c28c0ae2a446a669e6290bfea7e79b2
Nikita Lozhnikov <nikitml@gmail.com>
2019-10-07 23:08:35 +0300

    extract string tokenize

--- src/main/scala/implicits.scala
+++ src/main/scala/implicits.scala
@@ -2,5 +2,7 @@ object implicits {
 
   implicit class stringUtils(s: String) {
     def sanitizeTrimLower: String = s.replaceAll("""('s)|([\p{Punct}&&[^-]])""", " ").trim.toLowerCase
+
+    def tokenize: Array[String] = s.split("\\s").map(_.sanitizeTrimLower).filter(_.length > 1)
   }
 }
